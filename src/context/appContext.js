6d593eb311aa781459010286bcc175e0333b130a
hasaan <hassaankhan5550@gmail.com>
2023-01-29 19:32:23 +0500

    third

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,5 +1,6 @@
 import {io} from 'socket.io-client';
 import React from 'react';
-const SOCKET_URL = 'http://localhost:5001';
+// const SOCKET_URL = 'http://localhost:5001';
+const SOCKET_URL = 'https://backend-chat-app-self.vercel.app';
 export const socket = io(SOCKET_URL);
 export const AppContext = React.createContext();
